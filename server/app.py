4159b0cd48eb6464b0e95ea0fafa90cc39c24f7f
Erdem Yilmaz <erdemy885@gmail.com>
2024-09-13 22:27:24 -0400

    register posts blueprint

--- server/app.py
+++ server/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from posts import posts_bp
+app.register_blueprint(posts_bp)
+
 @app.route('/', methods=["GET"])
 def hello_world():
     return "hello world"
